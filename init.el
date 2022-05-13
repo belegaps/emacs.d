cc24207cc2aa6d9f96765a878a63faddbf66b1af
Per Salomonsen <psalomonsen@belega.dk>
2022-05-13 10:20:25 +0200

    Initial commit

---
+++ init.el
@@ -0,0 +1,10 @@
+;; Mac OS X Keyboard Binding
+(global-set-key (kbd "M-(") "{")
+(global-set-key (kbd "M-)") "}")
+(global-set-key (kbd "M-8") "[")
+(global-set-key (kbd "M-9") "]")
+(global-set-key (kbd "M-'") "@")
+
+;; Start server
+(server-start)
+

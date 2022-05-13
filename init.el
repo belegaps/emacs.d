9995a20468d655867f68b2a0dd0aac1873fc3121
Per Salomonsen <psalomonsen@belega.dk>
2022-05-13 10:27:24 +0200

    Map 'server-edit' to 'C-x C-3' to keep editing tempo

--- init.el
+++ init.el
@@ -6,6 +6,11 @@
 (global-set-key (kbd "M-'") "@")
 (global-set-key (kbd "M-i") "|")
 
+;; Custom keyboard bindings
+
+;; Alternative to "C-x #" to keep editing tempo
+(global-set-key (kbd "C-x C-3") 'server-edit)
+
 ;; Start server
 (server-start)
 

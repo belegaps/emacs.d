e94f8e11decf8a6f5f58d841c373afff96d67db9
Per Salomonsen <psalomonsen@belega.dk>
2022-05-13 11:09:57 +0200

    Install nord-theme and customize default font height

--- init.el
+++ init.el
@@ -1,3 +1,9 @@
+;; Configure package manager
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
 ;; Mac OS X Danish Keyboard Bindings
 (global-set-key (kbd "M-(") "{")
 (global-set-key (kbd "M-)") "}")
@@ -14,3 +20,15 @@
 ;; Start server
 (server-start)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(nord-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo")))))

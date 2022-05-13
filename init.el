8195c14ab971ddac3894a746bf0d5ccd9c946157
Per Salomonsen <psalomonsen@belega.dk>
2022-05-13 10:25:23 +0200

    Add keyboard binding for '|' character

--- init.el
+++ init.el
@@ -1,9 +1,10 @@
-;; Mac OS X Keyboard Binding
+;; Mac OS X Danish Keyboard Bindings
 (global-set-key (kbd "M-(") "{")
 (global-set-key (kbd "M-)") "}")
 (global-set-key (kbd "M-8") "[")
 (global-set-key (kbd "M-9") "]")
 (global-set-key (kbd "M-'") "@")
+(global-set-key (kbd "M-i") "|")
 
 ;; Start server
 (server-start)

f3a5415281328321c41eb88f1696d2e0fc84a686
Per Salomonsen <psalomonsen@belega.dk>
2022-05-16 12:09:41 +0200

    Install `yaml-mode` package.
    Also, fix keyboard mapping of '\' character and collect generated
    files under `.emacs.d/.gen/` to ignore them collectively in git.

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
 (global-set-key (kbd "M-9") "]")
 (global-set-key (kbd "M-'") "@")
 (global-set-key (kbd "M-i") "|")
+(global-set-key (kbd "M-/") "\\")
 
 ;; User Interface
 (tool-bar-mode -1)
@@ -31,10 +32,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-list-file-prefix "~/.emacs.d/.gen/auto-save-list/.saves-")
  '(custom-enabled-themes '(nord))
  '(custom-safe-themes
    '("197cefea731181f7be51e9d498b29fb44b51be33484b17416b9855a2c4243cb1" default))
- '(package-selected-packages '(nord-theme)))
+ '(package-selected-packages '(yaml-mode nord-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

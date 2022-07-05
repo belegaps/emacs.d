f4b3e02d3b97eb2193577f25c838ef52579fee48
Per Salomonsen <psalomonsen@belega.dk>
2022-07-05 13:04:38 +0200

    Fix init order for projectile
    Initializing projectile before customizing location of file for known
    projects caused it to write an empty project file in the default
    location.

--- init.el
+++ init.el
@@ -19,12 +19,6 @@
 (add-to-list 'default-frame-alist '(height . 43))
 (add-to-list 'default-frame-alist '(width . 132))
 
-;; Projectile
-(projectile-mode +1)
-
-;; Custom keyboard bindings
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-
 ;; Alternative to "C-x #" to keep editing tempo
 (global-set-key (kbd "C-x C-3") 'server-edit)
 
@@ -40,10 +34,19 @@
  '(custom-enabled-themes '(nord))
  '(custom-safe-themes
    '("197cefea731181f7be51e9d498b29fb44b51be33484b17416b9855a2c4243cb1" default))
- '(package-selected-packages '(projectile magit go-mode lua-mode yaml-mode nord-theme)))
+ '(package-selected-packages '(projectile magit go-mode lua-mode yaml-mode nord-theme))
+ '(projectile-known-projects-file
+   (expand-file-name "transient/projectile-bookmarks.eld" user-emacs-directory)))
+
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :extend nil :stipple nil :background "#2E3440" :foreground "#D8DEE9" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo")))))
+
+;; Projectile
+(projectile-mode +1)
+
+;; Custom keyboard bindings
+(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)

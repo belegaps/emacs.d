374b8a11ae0724ad787d9eabc90837872d6cb28e
Per Salomonsen <psalomonsen@belega.dk>
2022-07-30 09:54:22 +0200

    Separate customizations into separate file

---
+++ emacs-custom.el
@@ -0,0 +1,9 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-save-list-file-prefix "~/.emacs.d/.gen/auto-save-list/.saves-")
+ '(package-selected-packages
+   '(org-bullets go-mode lua-mode magit nord-theme typescript-mode use-package which-key yaml-mode))
+ '(safe-local-variable-values '((user-mail-address . "psalomonsen@belega.dk"))))
--- init.el
+++ init.el
@@ -1,12 +1,5 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(auto-save-list-file-prefix "~/.emacs.d/.gen/auto-save-list/.saves-")
- '(package-selected-packages
-   '(org-bullets go-mode lua-mode magit nord-theme typescript-mode use-package which-key yaml-mode))
- '(safe-local-variable-values '((user-mail-address . "psalomonsen@belega.dk"))))
+(setq custom-file (expand-file-name "emacs-custom.el" user-emacs-directory))
+(load custom-file)
 
 ;; Load configuration from ~/.emacs.d/config.org
 (org-babel-load-file

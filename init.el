793f120792789a78ba7d693835de93ddd4b98cb3
Per Salomonsen <psalomonsen@belega.dk>
2022-07-30 11:04:44 +0200

    Move customization config into `config.org`

--- init.el
+++ init.el
@@ -1,6 +1,3 @@
-(setq custom-file (expand-file-name "emacs-custom.el" user-emacs-directory))
-(load custom-file)
-
 ;; Load configuration from ~/.emacs.d/config.org
 (org-babel-load-file
  (expand-file-name

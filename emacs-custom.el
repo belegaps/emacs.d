abb221e977b3ade55afa5148100a3659353b3bbc
Per Salomonsen <psalomonsen@belega.dk>
2022-10-10 21:29:21 +0200

    Add support for gherkin features

--- emacs-custom.el
+++ emacs-custom.el
@@ -5,5 +5,20 @@
  ;; If there is more than one, they won't work right.
  '(auto-save-list-file-prefix "~/.emacs.d/.gen/auto-save-list/.saves-")
  '(package-selected-packages
-   '(projectile-ripgrep org-bullets go-mode lua-mode magit nord-theme typescript-mode use-package which-key yaml-mode))
+   '(feature-mode projectile-ripgrep org-bullets go-mode lua-mode magit nord-theme typescript-mode use-package which-key yaml-mode))
  '(safe-local-variable-values '((user-mail-address . "psalomonsen@belega.dk"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:height 150))))
+ '(org-document-title ((t (:inherit default :weight bold :foreground "#D8DEE9" :font "PT Sans" :height 2.0 :underline nil))))
+ '(org-level-1 ((t (:inherit default :weight bold :foreground "#D8DEE9" :font "PT Sans" :height 1.75))))
+ '(org-level-2 ((t (:inherit default :weight bold :foreground "#D8DEE9" :font "PT Sans" :height 1.5))))
+ '(org-level-3 ((t (:inherit default :weight bold :foreground "#D8DEE9" :font "PT Sans" :height 1.25))))
+ '(org-level-4 ((t (:inherit default :weight bold :foreground "#D8DEE9" :font "PT Sans" :height 1.1))))
+ '(org-level-5 ((t (:inherit default :weight bold :foreground "#D8DEE9" :font "PT Sans"))))
+ '(org-level-6 ((t (:inherit default :weight bold :foreground "#D8DEE9" :font "PT Sans"))))
+ '(org-level-7 ((t (:inherit default :weight bold :foreground "#D8DEE9" :font "PT Sans"))))
+ '(org-level-8 ((t (:inherit default :weight bold :foreground "#D8DEE9" :font "PT Sans")))))
